eb9dea974705cd1f98ffc4dd74fc1ead741f2b1d
smooto <melissasmoot@live.com>
2020-04-13 15:49:09 -0700

    init product data

---
+++ data/product-list.js
@@ -0,0 +1,104 @@
+const allProducts = [
+    {
+        id: 'P01',
+        image: 'assets/bag.jpg',
+        name: 'bag'
+    },
+    {
+        id: 'P02',
+        image: 'assets/banana.jpg',
+        name: 'banana'
+    },
+    {
+        id: 'P03',
+        image: 'assets/bathroom.jpg',
+        name: 'bathroom'
+    },
+    {
+        id: 'P04',
+        image: 'assets/boots.jpg',
+        name: 'boots'
+    },
+    {
+        id: 'P05',
+        image: 'assets/breakfast.jpg',
+        name: 'breakfast'
+    },
+    {
+        id: 'P06',
+        image: 'assets/bubblegum.jpg',
+        name: 'bubblegum'
+    },
+    {
+        id: 'P07',
+        image: 'assets/chair.jpg',
+        name: 'chair'
+    },
+    {
+        id: 'P08',
+        image: 'assets/cthulhu.jpg',
+        name: 'cute-thulhu'
+    },
+    {
+        id: 'P09',
+        image: 'assets/dog-duck.jpg',
+        name: 'dog-duck'
+    },
+    {
+        id: 'P10',
+        image: 'assets/dragon.jpg',
+        name: 'dragon'
+    },
+    {
+        id: 'P11',
+        image: 'assets/pen.jpg',
+        name: 'pen'
+    },
+    {
+        id: 'P12',
+        image: 'assets/pet-sweep.jpg',
+        name: 'pet sweep'
+    },
+    {
+        id: 'P13',
+        image: 'assets/scissors.jpg',
+        name: 'scissors'
+    },
+    {
+        id: 'P14',
+        image: 'assets/shark.jpg',
+        name: 'shark'
+    },
+    {
+        id: 'P15',
+        image: 'assets/sweep.png',
+        name: 'sweep'
+    },
+    {
+        id: 'P16',
+        image: 'assets/tauntaun.jpg',
+        name: 'tauntaun'
+    },
+    {
+        id: 'P17',
+        image: 'assets/unicorn.jpg',
+        name: 'unicorn'
+    },
+    {
+        id: 'P18',
+        image: 'assets/usb.gif',
+        name: 'usb'
+    },
+    {
+        id: 'P19',
+        image: 'assets/water-can.jpg',
+        name: 'watering can'
+    },
+    {
+        id: 'P20',
+        image: 'assets/wine-glass.jpg',
+        name: 'wine glass'
+    }
+];
+
+export default allProducts;
